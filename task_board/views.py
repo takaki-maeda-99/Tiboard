e1f159fcdc5d8e043b5364508b65619080bdb00c
maeda-tkk <taka.king1911@icloud.com>
2024-08-22 14:53:22 +0900

    db1

--- task_board/views.py
+++ task_board/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+
+from .models import User, Course
 
 # Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        print(Course.objects.filter(user__user_id="105125872991741815222"))
+        return render(request, "task_board/index.html")
+
+#Viewクラスを継承したIndexViewをas_viewメソッドでビュー関数に変換
+index = IndexView.as_view()
