75590277ffc5c3398b058d1327998a7b5a9dff99
sNAGUs256 <naoki161015@gmail.com>
2024-09-18 09:51:01 +0900

    auto create threads

---
+++ question_board/management/commands/create_threads.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from question_board.models import Thread
+from task_board.models import Course
+
+class Command(BaseCommand):
+    help = 'Create threads for all existing courses if not already created.'
+
+    def handle(self, *args, **kwargs):
+        courses = Course.objects.all()
+        for course in courses:
+            if not Thread.objects.filter(course=course).exists():
+                Thread.objects.create(course=course, description=f"Thread for {course.course_name}")
+                self.stdout.write(self.style.SUCCESS(f"Created Thread for course: {course.course_name}"))
+            else:
+                self.stdout.write(self.style.WARNING(f"Thread already exists for course: {course.course_name}"))
