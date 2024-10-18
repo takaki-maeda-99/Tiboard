d9575a2687539d38d9c1664efd37f412158f4ef8
sNAGUs256 <naoki161015@gmail.com>
2024-10-18 14:20:53 +0900

    update slidebar,color and tboard,made sidebar func

--- static/base/script.js
+++ static/base/script.js
@@ -1,5 +1,5 @@
-let COLOR_HEAD_MAIN = '#0b4e9bc0'
-let COLOR_HEAD_SIDE = '#0b4e9b80'
+let COLOR_HEAD_MAIN = '#26282c'
+let COLOR_HEAD_SIDE = '#26282cf0'
 let COLOR_CONTENT_MAIN = '#b5e2fa'
 let COLOR_CONTENT_SIDE = '#b5e2fa80'
 let COLOR_ACCENT = '#913e0b'
@@ -7,13 +7,13 @@ let COLOR_ACCENT = '#913e0b'
 function setDefaultColor() {
     document.getElementById('head-bar-main').style.backgroundColor = COLOR_HEAD_MAIN;
     document.getElementById('head-bar-side').style.backgroundColor = COLOR_HEAD_SIDE;
-    document.getElementById('content-main').style.backgroundColor = COLOR_CONTENT_MAIN;
-    document.getElementById('content-side').style.backgroundColor = COLOR_CONTENT_SIDE;
+    document.getElementById('content-main').style.backgroundColor = COLOR_HEAD_MAIN;
+    document.getElementById('content-side').style.backgroundColor = COLOR_HEAD_SIDE;
 
-    document.getElementById('head-bar-main').style.border = `solid 1px ${COLOR_ACCENT}`;
-    document.getElementById('head-bar-side').style.border = `solid 1px ${COLOR_ACCENT}`;
-    document.getElementById('content-main').style.border = `solid 1px ${COLOR_ACCENT}`;
-    document.getElementById('content-side').style.border = `solid 1px ${COLOR_ACCENT}`;
+    // document.getElementById('head-bar-main').style.border = `solid 1px ${COLOR_ACCENT}`;
+    // document.getElementById('head-bar-side').style.border = `solid 1px ${COLOR_ACCENT}`;
+    // document.getElementById('content-main').style.border = `solid 1px ${COLOR_ACCENT}`;
+    // document.getElementById('content-side').style.border = `solid 1px ${COLOR_ACCENT}`;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
     if (toggleButton && sidebar) {
         toggleButton.addEventListener('click', function () {
             sidebar.classList.toggle('show');
-            toggleButton.textContent = sidebar.classList.contains('show') ? '<' : '>';
         });
     }
 
@@ -34,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
             if (sidebar.classList.contains('show')) {
                 sidebar.classList.remove('show');
-                toggleButton.textContent = '>';
             }
         }
     });
@@ -43,7 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
             if (sidebar.classList.contains('show')) {
                 sidebar.classList.remove('show');
-                toggleButton.textContent = '>';
             }
         }
     });
