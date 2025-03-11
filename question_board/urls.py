0641cc5fdaa0ff92773f8d302ec978484eaf8a96
N. Taniguchi <naoki161015@gmail.com>
2025-03-11 17:05:44 +0900

    うまくできてるかわからん

--- question_board/urls.py
+++ question_board/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 app_name = "question_board"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('thread_list/', views.thread_list, name='thread_list'),
+    path('get_thread/<int:thread_id>', views.get_thread, name='get_thread'),
     path('thread/<int:thread_id>/', views.thread_detail, name='thread_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
